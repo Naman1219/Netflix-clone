33aa85c967e5b57a4c9eecf8670f6c6391204e8b
naman <guptanaman1995@gmail.com>
2024-01-08 22:08:21 +0530

    Completed basic header code setup

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -1,6 +1,25 @@
-
+import Image from "next/image"
 export default function Header() {
-  return (
-    <div>Header</div>
+  return ( <header>
+    <div className="flex items-center space-x-2 md:space-x-10">
+    <Image
+          // https://rb.gy/ulxxee
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          width={100}
+          alt="netflix-logo"
+          height={100}
+          className="cursor-pointer object-contain"
+          priority={true}
+        />
+      <ul className="hidden space-x-4 md:flex">
+        <li className="headerLink">Home</li>
+        <li className="headerLink">TV Shows</li>
+        <li className="headerLink">Movies</li>
+        <li className="headerLink">New & Popular</li>
+        <li className="headerLink">My List</li>
+      </ul>
+    </div>
+    <div></div>
+  </header>
   )
 };
