b78822ed06383297ca5ee90f2224aa05028f0d33
naman <guptanaman1995@gmail.com>
2024-01-10 12:35:03 +0530

    Corrected server side rendering props according to nextjs 14

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 }
 
 
-export const getServerSideProps = async () => {
+export const getServerProps = async () => {
   const [
     netflixOriginals,
     trendingNow,
