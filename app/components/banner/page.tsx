2b6bde6bd5d4771e227a04c0869a4d35828fc39e
naman <guptanaman1995@gmail.com>
2024-01-12 20:32:03 +0530

    Added Banner component and changed nextjs config file

--- app/components/banner/page.tsx
+++ app/components/banner/page.tsx
@@ -1,11 +1,31 @@
+"use client"
 import { Movie } from "@/typings";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { baseUrl } from "@/constants/movie";
 interface Props{
   netflixOriginals : Movie[];
 }
 
 function Banner({netflixOriginals}: Props) {
+
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() =>{
+    setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
+  },[netflixOriginals]);
+  
   return (
-    <div></div>
+    <div>
+      <div className="absolute top-0 left-0 h-[95vh] w-screen">
+    <Image
+      src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+      alt="banner-image"
+      layout="fill"
+      objectFit="cover"
+    />
+      </div>
+    </div>
   )
 }
 
