2624c944a738cdb50e0505b0f709b285cefd0d3c
naman <guptanaman1995@gmail.com>
2024-01-27 23:35:42 +0530

    Added all rows and scroll bar lines in css

--- app/page.tsx
+++ app/page.tsx
@@ -63,18 +63,20 @@ export const getMovies = async () => {
       documentaries
     } = movieData;
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10
-    to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          {/* <Row />
-          <Row />
-          <Row />
-          <Row />
-          <Row />
-          <Row /> */}
+        <section className="md:space-y-24">
+        <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
