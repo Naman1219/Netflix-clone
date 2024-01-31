884f88e48be7e78522ac0edb90b058928b42be77
naman <guptanaman1995@gmail.com>
2024-01-31 16:31:21 +0530

    Build login page and its functionalities

--- next.config.js
+++ next.config.js
@@ -3,12 +3,16 @@
 module.exports = {
   reactStrictMode: true,
   images: {
+    dangerouslyAllowSVG: true,
     remotePatterns: [
       {
         hostname: 'image.tmdb.org'
       },
       {
         hostname: 'upload.wikimedia.org'
+      },
+      {
+        hostname: 'rb.gy'
       }
     ]
   }
